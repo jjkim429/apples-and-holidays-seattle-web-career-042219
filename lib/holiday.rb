0b54d8a44505ae28c1904fc1684b36e7a21d3f1a
Jennifer Kim <jjkim429@gmail.com>
2019-04-15 21:52:22 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -72,7 +72,7 @@ def all_holidays_with_bbq(holiday_hash)
   holiday_hash.each do |season, holiday|
     holiday.each do |attribute, value|
       if value == "BBQ"
-        
+        holiday_hash.key
 end
 
 

d7762866743da4bf1b9cbcfb5293c0f3e88bf4c4
Jennifer Kim <jjkim429@gmail.com>
2019-04-15 21:53:47 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -69,9 +69,9 @@ def all_holidays_with_bbq(holiday_hash)
   # return an array of holiday names (as symbols) where supply lists
   # include the string "BBQ"
   
-  holiday_hash.each do |season, holiday|
-    holiday.each do |attribute, value|
-      if value == "BBQ"
+  holiday_hash.map do |season, holiday|
+    holiday.each do |holiday, supplies|
+      if value == 
         holiday_hash.keys
       end
     end

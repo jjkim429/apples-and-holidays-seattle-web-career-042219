27c0ea4180d77d633ccbe967d8af229959d48cbe
Jennifer Kim <jjkim429@gmail.com>
2019-04-15 21:40:06 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -59,7 +59,7 @@ def all_supplies_in_holidays(holiday_hash)
   # etc.
   holiday_hash.each do |season, data|
     data.each do |holiday, supplies|
-      puts "#{holiday}: "      
+      puts "#{holiday}: #{supplies}"      
 end
 
 def all_holidays_with_bbq(holiday_hash)

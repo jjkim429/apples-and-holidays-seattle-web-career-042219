2c29687847ab1ee3f524654c39db6563830be4ce
Jennifer Kim <jjkim429@gmail.com>
2019-04-15 21:30:57 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -25,7 +25,13 @@ def add_supply_to_winter_holidays(holiday_hash, supply)
   # holiday_hash is identical to the one above
   # add the second argument, which is a supply, to BOTH the
   # Christmas AND the New Year's arrays
-holiday_hash[:winter][:christmas, :new_years] << supply
+  holiday_hash[:winter].each do |attribute, value|
+    attribute >> supply
+ contacts["Freddy Mercury"].each do |attribute, value|
+    if attribute == :favorite_icecream_flavors
+      value.delete_if { |flavor| flavor == "strawberry" }
+      end
+    end
 end
 
 
